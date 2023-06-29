7fd0ad2fd19d00f2be0afc6d98e0bfa96cb36c9f
rahulf777 <138130802+rahulf777@users.noreply.github.com>
2023-06-29 18:35:42 +0300

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,34 @@
+(() => {
+    "use strict";
+    function isWebp() {
+        function testWebP(callback) {
+            let webP = new Image;
+            webP.onload = webP.onerror = function() {
+                callback(webP.height == 2);
+            };
+            webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+        }
+        testWebP((function(support) {
+            let className = support === true ? "webp" : "no-webp";
+            document.documentElement.classList.add(className);
+        }));
+    }
+    let addWindowScrollEvent = false;
+    setTimeout((() => {
+        if (addWindowScrollEvent) {
+            let windowScroll = new Event("windowScroll");
+            window.addEventListener("scroll", (function(e) {
+                document.dispatchEvent(windowScroll);
+            }));
+        }
+    }), 0);
+    let x;
+    document.getElementById("rollButton").onclick = function() {
+        x = Math.random() * 6 + 1 ;
+        x = (Math.round(x * 100) / 100).toFixed(2);
+        document.getElementById("xlabel").innerHTML = x;
+    };
+    document.querySelector(".button");
+    window["FLS"] = true;
+    isWebp();
+})();
